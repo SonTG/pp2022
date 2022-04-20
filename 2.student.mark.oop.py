58e303171002d161e98383ed02188a0648e45c3f
3Eye18 <ngvu182002@gmail.com>
2022-04-20 23:23:14 +0700

    second commit: fix file

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,56 +1,70 @@
-#storing info
-Students=[]
-Courses=[]
-class student:
-    #Store
-    def __init__(self, ID = None, name = None, DoB = None, course_name = None, course_mark = None):
-        self.name = name
-        self.id = ID
-        self.date = DoB
-        self.coursename = course_name
-        self.coursemark = course_mark
-    
-    #Add
-    def addInfo(self):
-        self.name = input("Input name: ")
-        self.id = input("Input ID: ")
-        self.date = input("Input DoB: ")
+class Course:
+    def __init__(self, name, mark, number):
+        self._name = name
+        self._mark = mark
+        self._number = number
 
-    #Describe
-    def list(self):
-        print("-Name:",self.name)
-        print("-ID:",self.id)
-        print("-DoB:",self.date)
+    def get_name(self):
+        return self._name
 
-    #Course info
-    def setCourse(self):
-        self.coursename = input("Enter course name:")
-        self.coursemark = input("Enter course mark:")
-    
-    #List course
-    def listCourse(self):
-        print("-Course name:",self.coursename)
-        print("-Course mark:",self.coursemark)
-        
-  
-#driver
-a=int(input("Numbers of students:"))
-for i in range(0,a):
-    Student=student()
-    Student.addInfo()
-    Students.append(Student)
-    b=int(input("Numbers of courses:"))
-    for n in range(0,b):
-        Student=student()
-        Student.setCourse()
-        Courses.append(Student)
-    print("")
+    def get_mark(self):
+        return self._mark
 
-print("Student's info list:")   
-for Student in Students:
-    Student.list()
-    print("Student's course info:")
-    for Student in Courses:
-        Student.listCourse()
-        print("")
-    print("")
+    def get_number(self):
+        return self._number
+
+class Student:
+    def __init__(self, student_id, name, dob):
+        self._id = student_id
+        self._name = name
+        self._dob = dob
+        self._course_list = []
+
+    def set_course(self, course_list):
+        self._course_list = course_list
+
+    def print_student_data(self):
+        print()
+        print("-Name: ", self._name)
+        print("-ID: ", self._id)
+        print("-DoB: ", self._dob)
+
+    def print_course_data(self):
+        for course in self._course_list:
+            print("-Course name: ", course.get_name())
+            print("-Course mark: ", course.get_mark())
+            print("-Course number: ", course.get_number())
+            print()
+
+def input_student(student_count):
+    student_list = []
+    for i in range(student_count):
+        name = input("Name:")
+        student_id = input("ID:")
+        dob = input("DoB:")
+        student_list.append(Student(student_id, name, dob))
+    return student_list
+
+def input_course(course_count):
+    course_list = []
+    for i in range(course_count):
+        course_name = input("Enter course name:")
+        course_mark = input("Enter course mark:")
+        course_number = input("Enter course number:")
+        course_list.append(Course(course_name,course_mark,course_number))
+    return course_list
+
+def main():
+    student_count = int(input("Numbers of students: "))
+    student_list = input_student(student_count)
+
+    for student in student_list:
+        course_count = int(input("Numbers of courses: "))
+        course_list = input_course(course_count)
+        student.set_course(course_list)
+
+    for student in student_list:
+        student.print_student_data()
+        student.print_course_data()
+
+main()
